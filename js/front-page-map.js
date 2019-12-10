110a0bdfacfe8bc5f4b62769fd11a8b5606426cd
Jason Stidd <jason.stidd@utah.edu>
2019-12-10 15:53:59 -0700

    map changes

--- js/front-page-map.js
+++ js/front-page-map.js
@@ -1,7 +1,13 @@
-var map = L.map('map').setView([25, -10], 1.35);
+var map = L.map('map').setView([40, -50], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+var slateIcon = L.icon({
+    iconUrl: '/css/images/marker-icon.png',
+    iconSize: [15, 25],
+    iconAnchor: [7, 24]
+})
 
 axios.get("https://slate-geoip.s3.amazonaws.com/map_data")
     .then(function (response) {
@@ -13,7 +19,7 @@ axios.get("https://slate-geoip.s3.amazonaws.com/map_data")
             if (organization != null
                 && lat != null
                 && lon != null) {
-                L.marker([lat, lon])
+                L.marker([lat, lon], {icon: slateIcon})
                     .addTo(map)
                     .bindPopup(organization + " / " + name);
             }
